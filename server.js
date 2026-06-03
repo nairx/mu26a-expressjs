872e60e56b9275ae81f40f7ac9a571350e3b3f45
nairx <nairxprv@gmail.com>
2026-06-03 13:10:06 +0530

    updated

--- server.js
+++ server.js
@@ -5,14 +5,30 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const products = [
-    { id: 1, name: "Product 1", price: 23 },
-    { id: 2, name: "Product 2", price: 70 },
-    { id: 3, name: "Product 3", price: 50 },
-]
+const productSchema = mongoose.Schema({
+    name: { type: String },
+    price: { type: Number }
+})
 
-app.get("/products", (req, res) => {
+const productModel = mongoose.model("products", productSchema)
+
+const startServer = async () => {
+    await mongoose.connect("mongodb://localhost:27017/mu26a")
+    app.listen(5000, () => console.log("Server Started"))
+}
+
+startServer()
+
+// const products = [
+//     { id: 1, name: "Product 1", price: 23 },
+//     { id: 2, name: "Product 2", price: 70 },
+//     { id: 3, name: "Product 3", price: 50 },
+// ]
+
+app.get("/products", async (req, res) => {
+    const products = await productModel.find()
     res.json(products)
 })
 
-app.listen(5000,()=>console.log("Server Started"))
+
+
