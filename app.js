f07c5ffbab53888981ee472035c98900e380202e
nairx <nairxprv@gmail.com>
2026-06-08 22:39:19 +0530

    updated

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from "express"
 import cors from "cors"
 import userRoutes from "./routes/userRoutes.js"
+import productRoutes from "./routes/productRoutes.js"
 const app = express();
 app.use(express.json());
 app.use(cors())
 app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
 export default app
