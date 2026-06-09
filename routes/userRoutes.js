54e037c8dc453bd70a39c511836de8097287ddf4
nairx <nairxprv@gmail.com>
2026-06-09 13:01:15 +0530

    updated

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,6 @@ import express from "express"
 import { authenticate,authorize } from "../middleware/authMiddleware.js";
 import * as userController from "../controllers/userController.js"
 const router = express.Router();
-
 router.get("/get", authenticate, authorize("admin"), userController.getUsers);
 router.get("/getUser/:userId",userController.getUser)
 router.post("/login", userController.authUser);
